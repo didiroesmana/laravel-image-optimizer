6cf834f4c319fb888f4d406239f7cabad64496ba
Didi Hadya Roesmana <didileeee@gmail.com>
2017-11-02 16:24:35 +0700

    Update AutoImageOptimizer.php

--- src/Middleware/AutoImageOptimizer.php
+++ src/Middleware/AutoImageOptimizer.php
@@ -21,7 +21,7 @@ class AutoImageOptimizer
             /** @var \Approached\LaravelImageOptimizer\ImageOptimizer $imageOptimizer */
             $imageOptimizer = app('Approached\LaravelImageOptimizer\ImageOptimizer');
 
-            foreach ($request->all() as $one) {
+            foreach ($request->file() as $one) {
                 if (is_array($one)) {
                     foreach ($one as $file) {
                         if ($this->isImageFile($file)) {
